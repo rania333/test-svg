0b11fddb3a6edd245043cc6de411d2b527c0c1be
rania333 <raniaaramadan1999@gmail.com>
2023-02-15 21:40:37 +0200

    test npm svg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { SvgIconRegistryService } from 'angular-svg-icon/public-api';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'svg-test';
+  // constructor(public iconReg:SvgIconRegistryService) {
+    // iconReg.loadSvg('one.svg')?.subscribe((res => console.log(res)));
+  // }
+
+  public form!: FormGroup;
+  constructor( private _fb: FormBuilder){
+    this.form = this._fb.group({
+      color: [null],
+      opacity: [.5],
+      padding: [2],
+      width: [2],
+      height: [2]
+    })
+  }
+
+  test() {
+    console.log('test', this.form.value)
+  }
+
+  get formControl() {
+    return this.form.controls
+  }
+
+  getNgStyle() {
+		const style: any = {};
+
+		if (!this.formControl['height'].value) {
+			style['height.px'] = 100;
+		}
+
+		if (!this.formControl['width'].value) {
+			style['width.px'] = 100;
+		}
+
+		style.fill = this.formControl['color'].value;
+
+		if (this.formControl['padding']) {
+			style['padding.px'] = this.formControl['padding'].value;
+		}
+
+		if (this.formControl['opacity'].value) {
+			style.opacity = this.formControl['opacity'].value;
+		}
+
+		return style;
+	}
 }
